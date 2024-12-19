8c4546e9a2093b73a7065ad007161cb915137dd7
Nishant-Ray <raynishant1@gmail.com>
2024-12-18 20:35:49 -0800

    Main layout ui adjustments

--- vault-frontend/app/(main)/layout.tsx
+++ vault-frontend/app/(main)/layout.tsx
@@ -1,8 +1,21 @@
-import Link from "next/link";
+'use client';
+
 import { dmSans } from "../ui/fonts";
 import SideNav from "../ui/sideNav";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const titles: Record<string, string> = {
+    '/dashboard': 'Dashboard',
+    '/spending': 'Spending',
+    '/wallet': 'Wallet',
+    '/bills': 'Bills',
+    '/residence': 'Residence',
+    '/chatbot': 'Chatbot',
+    '/settings': 'Settings',
+  };
+
   return (
     <div>
       <div className="flex flex-row">
@@ -10,15 +23,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SideNav/>
         </div>
 
-        <div className="w-full">
-          <div className="bg-white px-5 h-20 flex flex-row justify-end items-center gap-x-6">
-            <div className="w-10 h-10 bg-green-400"/>
-            <div className="w-10 h-10 bg-orange-400"/>
+        <div className="flex flex-col w-full">
+          
+          <div className="w-full flex flex-row h-20 items-center px-5 bg-white">
+            <div className="w-full h-full flex justify-start items-center">
+              <h1 className={`${dmSans.className} antialiased tracking-tighter text-off_black text-4xl font-bold`}>{titles[pathname]}</h1>
+            </div>
+
+            <div className="w-full h-full flex flex-row justify-end items-center gap-x-6">
+              <div className="w-10 h-10 bg-green-400"/>
+              <div className="w-10 h-10 bg-orange-400"/>
+            </div>  
           </div>
 
           <div className="w-full bg-off_white">{children}</div>
         </div>
-
       </div>
     </div>
   );
